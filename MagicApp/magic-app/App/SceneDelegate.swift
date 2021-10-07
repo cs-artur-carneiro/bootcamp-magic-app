cec5c1cd1711a7f132a5132ff35261b22f9f3518
Artur Carneiro <91500022+cs-artur-carneiro@users.noreply.github.com>
2021-10-07 13:27:35 -0300

    Add temp TabBar

--- MagicApp/magic-app/App/SceneDelegate.swift
+++ MagicApp/magic-app/App/SceneDelegate.swift
@@ -11,15 +11,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
+        let tabBarController = UITabBarController(nibName: nil, bundle: nil)
+        tabBarController.tabBar.barTintColor = .clear
+        tabBarController.tabBar.backgroundImage = UIImage()
+        tabBarController.tabBar.shadowImage = UIImage()
         let viewController = UINavigationController(rootViewController: SetsViewController())
-        window?.rootViewController = viewController
+        viewController.navigationBar.prefersLargeTitles = true
+        viewController.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        viewController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        tabBarController.viewControllers = [viewController, UIViewController()]
+        tabBarController.selectedIndex = 0
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
 
